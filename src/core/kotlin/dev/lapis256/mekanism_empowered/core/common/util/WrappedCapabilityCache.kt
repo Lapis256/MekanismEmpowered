4269894cea99f71915ef7b3fa8953106644caf35
Lapis <45414333+Lapis256@users.noreply.github.com>
2025-12-24 05:57:04 +0900

    Fix fluid dupe caused by Inserter Upgrade

--- src/core/kotlin/dev/lapis256/mekanism_empowered/core/common/util/WrappedCapabilityCache.kt
+++ src/core/kotlin/dev/lapis256/mekanism_empowered/core/common/util/WrappedCapabilityCache.kt
@@ -2,6 +2,7 @@ package dev.lapis256.mekanism_empowered.core.common.util
 
 import mekanism.api.chemical.IChemicalHandler
 import mekanism.api.energy.IStrictEnergyHandler
+import mekanism.api.heat.IHeatHandler
 import mekanism.common.capabilities.Capabilities
 import mekanism.common.integration.energy.BlockEnergyCapabilityCache
 import mekanism.common.lib.transmitter.TransmissionType
@@ -11,52 +12,62 @@ import net.minecraft.server.level.ServerLevel
 import net.neoforged.neoforge.capabilities.BlockCapabilityCache
 import net.neoforged.neoforge.fluids.capability.IFluidHandler
 import net.neoforged.neoforge.items.IItemHandler
-import java.util.EnumMap
 
 
-sealed class WrappedCapabilityCache<CACHE, CAPABILITY : Any>(val type: TransmissionType) {
-    abstract fun createCache(level: ServerLevel, blockPos: BlockPos, side: Direction): CACHE
-    abstract fun getCapability(level: ServerLevel, blockPos: BlockPos, side: Direction): CAPABILITY?
-
-    val caches: MutableMap<Direction, CACHE> = EnumMap(Direction::class.java)
+/**
+ * キャッシュされた Capability をラップするクラス
+ */
+abstract class WrappedCapabilityCache<CAPABILITY> {
+    abstract val capability: CAPABILITY?
 
     companion object {
-        fun newCache(type: TransmissionType): WrappedCapabilityCache<out Any, out Any>? = when (type) {
-            TransmissionType.ITEM -> Item()
-            TransmissionType.CHEMICAL -> Chemical()
-            TransmissionType.FLUID -> Fluid()
-            TransmissionType.ENERGY -> Energy()
-            else -> error("unrecognized transmission type $type")
-        }
+        fun createFromTransmissionType(
+            type: TransmissionType,
+            level: ServerLevel,
+            blockPos: BlockPos,
+            context: Direction
+        ): WrappedCapabilityCache<*> =
+            when (type) {
+                TransmissionType.ITEM -> Item(level, blockPos, context)
+                TransmissionType.CHEMICAL -> Chemical(level, blockPos, context)
+                TransmissionType.FLUID -> Fluid(level, blockPos, context)
+                TransmissionType.ENERGY -> Energy(level, blockPos, context)
+                TransmissionType.HEAT -> Heat(level, blockPos, context)
+            }
+
     }
 
-    fun getCache(level: ServerLevel, blockPos: BlockPos, side: Direction): CACHE = caches.computeIfAbsent(side) { createCache(level, blockPos, side) }
-
-    class Item : WrappedCapabilityCache<BlockCapabilityCache<IItemHandler, Direction?>, IItemHandler>(TransmissionType.ITEM) {
-        override fun createCache(level: ServerLevel, blockPos: BlockPos, side: Direction): BlockCapabilityCache<IItemHandler, Direction?> =
-            Capabilities.ITEM.createCache(level, blockPos.relative(side), side.opposite)
-
-        override fun getCapability(level: ServerLevel, blockPos: BlockPos, side: Direction) = getCache(level, blockPos, side).capability
+    abstract class WrappedBlockCapabilityCache<CAPABILITY>(val cache: BlockCapabilityCache<CAPABILITY, Direction?>) :
+        WrappedCapabilityCache<CAPABILITY>() {
+        override val capability get() = cache.capability
     }
 
-    class Chemical : WrappedCapabilityCache<BlockCapabilityCache<IChemicalHandler, Direction?>, IChemicalHandler>(TransmissionType.CHEMICAL) {
-        override fun createCache(level: ServerLevel, blockPos: BlockPos, side: Direction): BlockCapabilityCache<IChemicalHandler, Direction?> =
-            Capabilities.CHEMICAL.createCache(level, blockPos.relative(side), side.opposite)
-
-        override fun getCapability(level: ServerLevel, blockPos: BlockPos, side: Direction) = getCache(level, blockPos, side).capability
+    class Item(cache: BlockCapabilityCache<IItemHandler, Direction?>) : WrappedBlockCapabilityCache<IItemHandler>(cache) {
+        constructor(level: ServerLevel, blockPos: BlockPos, context: Direction?) : this(Capabilities.ITEM.createCache(level, blockPos, context))
     }
 
-    class Fluid : WrappedCapabilityCache<BlockCapabilityCache<IFluidHandler, Direction?>, IFluidHandler>(TransmissionType.FLUID) {
-        override fun createCache(level: ServerLevel, blockPos: BlockPos, side: Direction): BlockCapabilityCache<IFluidHandler, Direction?> =
-            Capabilities.FLUID.createCache(level, blockPos.relative(side), side.opposite)
-
-        override fun getCapability(level: ServerLevel, blockPos: BlockPos, side: Direction) = getCache(level, blockPos, side).capability
+    class Chemical(cache: BlockCapabilityCache<IChemicalHandler, Direction?>) : WrappedBlockCapabilityCache<IChemicalHandler>(cache) {
+        constructor(level: ServerLevel, blockPos: BlockPos, context: Direction?) : this(Capabilities.CHEMICAL.createCache(level, blockPos, context))
     }
 
-    class Energy : WrappedCapabilityCache<BlockEnergyCapabilityCache, IStrictEnergyHandler>(TransmissionType.ENERGY) {
-        override fun createCache(level: ServerLevel, blockPos: BlockPos, side: Direction): BlockEnergyCapabilityCache =
-            BlockEnergyCapabilityCache.create(level, blockPos.relative(side), side.opposite)
+    class Fluid(cache: BlockCapabilityCache<IFluidHandler, Direction?>) : WrappedBlockCapabilityCache<IFluidHandler>(cache) {
+        constructor(level: ServerLevel, blockPos: BlockPos, context: Direction?) : this(Capabilities.FLUID.createCache(level, blockPos, context))
+    }
 
-        override fun getCapability(level: ServerLevel, blockPos: BlockPos, side: Direction) = getCache(level, blockPos, side).capability
+    class Heat(cache: BlockCapabilityCache<IHeatHandler, Direction?>) : WrappedBlockCapabilityCache<IHeatHandler>(cache) {
+        constructor(level: ServerLevel, blockPos: BlockPos, context: Direction?) : this(
+            BlockCapabilityCache.create(
+                Capabilities.HEAT,
+                level,
+                blockPos,
+                @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS") context
+            )
+        )
+    }
+
+    class Energy(val cache: BlockEnergyCapabilityCache) : WrappedCapabilityCache<IStrictEnergyHandler>() {
+        constructor(level: ServerLevel, blockPos: BlockPos, context: Direction?) : this(BlockEnergyCapabilityCache.create(level, blockPos, context))
+
+        override val capability get() = cache.capability
     }
 }
