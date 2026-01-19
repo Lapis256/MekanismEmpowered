cb531a417a1dae846c2794f45ac471745cbd654c
masyumero <167562220+masyumero@users.noreply.github.com>
2026-01-19 12:37:34 +0900

    Update EMExtras dependency to version 1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -355,7 +355,7 @@ val mainModDependencies = baseDependencies.toMutableList().apply {
     add(ModDep("mekanismelements", "2.3", ordering = Order.AFTER, mandatory = false))
     add(ModDep("mekanism_extras", "1.20.1-1.4.6", ordering = Order.AFTER, mandatory = false))
     add(ModDep("mekmm", "1.1.0", ordering = Order.AFTER, mandatory = false))
-    add(ModDep("emextras", "1.3.3", ordering = Order.AFTER, mandatory = false))
+    add(ModDep("emextras", "1.3.5", ordering = Order.AFTER, mandatory = false))
     add(ModDep("mekanismtweaks", "999.999.999-INCOMPATIBLE", false))
     add(ModDep("mekanismupgradesreborn", "999.999.999-INCOMPATIBLE", false))
 }
