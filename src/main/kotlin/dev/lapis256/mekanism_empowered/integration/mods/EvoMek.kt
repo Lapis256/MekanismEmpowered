ae6ed28a277507f08aba0c5a1a4379207fdc2d60
Lapis <45414333+Lapis256@users.noreply.github.com>
2026-03-05 02:13:10 +0900

    fix: Evolved Mekanism integration (#27)

--- src/main/kotlin/dev/lapis256/mekanism_empowered/integration/mods/EvoMek.kt
+++ src/main/kotlin/dev/lapis256/mekanism_empowered/integration/mods/EvoMek.kt
@@ -3,9 +3,12 @@ package dev.lapis256.mekanism_empowered.integration.mods
 import dev.lapis256.mekanism_empowered.common.init.MekEmpUpgrades.ITEM_IN_OUT_MACHINE_UPGRADES
 import dev.lapis256.mekanism_empowered.common.init.MekEmpUpgrades.addSupportedFactoryUpgrades
 import dev.lapis256.mekanism_empowered.core.common.util.AdditionalUpgradeUtil
+import dev.lapis256.mekanism_empowered.integration.IntegrationProviderRegistry
 import dev.lapis256.mekanism_empowered.integration.ModIntegration
+import dev.lapis256.mekanism_empowered.integration.provider.FactoryUpgradeIntegration
 import fr.iglee42.evolvedmekanism.registries.EMBlockTypes
 import fr.iglee42.evolvedmekanism.registries.EMFactoryType
+import fr.iglee42.evolvedmekanism.tiers.EMFactoryTier
 
 
 internal object EvoMek : ModIntegration {
@@ -18,5 +21,16 @@ internal object EvoMek : ModIntegration {
         AdditionalUpgradeUtil.addSupported(EMBlockTypes.CHEMIXER, *ITEM_IN_OUT_MACHINE_UPGRADES)
         AdditionalUpgradeUtil.addSupported(EMBlockTypes.MELTER, *ITEM_IN_OUT_MACHINE_UPGRADES)
         AdditionalUpgradeUtil.addSupported(EMBlockTypes.SOLIDIFIER, *ITEM_IN_OUT_MACHINE_UPGRADES)
+    }
+
+    val FACTORY_TIERS = arrayOf(EMFactoryTier.OVERCLOCKED, EMFactoryTier.QUANTUM, EMFactoryTier.DENSE, EMFactoryTier.MULTIVERSAL, EMFactoryTier.CREATIVE)
+
+    override fun initProvider(registry: IntegrationProviderRegistry) {
+        registry.registerProvider(FactoryUpgradeIntegration { type, upgrades ->
+            for (tier in FACTORY_TIERS) {
+                val blockType = EMBlockTypes.getFactory(tier, type) ?: continue
+                AdditionalUpgradeUtil.addSupported(blockType, *upgrades)
+            }
+        })
     }
 }
