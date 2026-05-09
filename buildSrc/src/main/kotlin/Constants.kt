6b5ff05fb39d8a3e83a9b81e18639a3e0ed6dba2
Lapis <45414333+Lapis256@users.noreply.github.com>
2026-05-09 19:08:19 +0900

    chore: bump version
    Version format changed from <MC Major>.<MC Minor>.<Mod Major>.<Mod Minor>.<Mod Patch> to <MC Major>.<MC Minor>-<Mod Major>.<Mod Minor>.<Mod Patch>.

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
         const val NAME = "Mekanism: Empowered"
         const val DESCRIPTION = "An addon mod that empowers Mekanism."
         const val LICENSE = "MIT"
-        const val VERSION = "21.1.1.1.2" // <MC Major>.<MC Minor>.<Mod Major>.<Mod Minor>.<Mod Patch>
+        const val VERSION = "21.1-1.2.0"
         const val GROUP = "dev.lapis256"
         const val AUTHOR = "Lapis256"
         const val REPOSITORY_URL = "https://github.com/Lapis256/MekanismEmpowered"
