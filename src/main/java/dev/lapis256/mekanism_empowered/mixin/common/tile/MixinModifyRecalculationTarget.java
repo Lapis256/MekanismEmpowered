95eb108fd6234cc8455a92b5e547626f3ff70d8d
Lapis <45414333+Lapis256@users.noreply.github.com>
2026-05-10 03:45:36 +0900

    fix(integration): add Large Pigment Mixer to recalculation target list

--- src/main/java/dev/lapis256/mekanism_empowered/mixin/common/tile/MixinModifyRecalculationTarget.java
+++ src/main/java/dev/lapis256/mekanism_empowered/mixin/common/tile/MixinModifyRecalculationTarget.java
@@ -57,6 +57,7 @@ class MixinModifyRecalculationTarget {
             "com.jerry.mekmm.common.tile.factory.TileEntityMoreMachineFactory",
             "com.jerry.meklm.common.tile.machine.TileEntityLargeChemicalInfuser",
             "com.jerry.meklm.common.tile.machine.TileEntityLargeElectrolyticSeparator",
+            "com.jerry.meklm.common.tile.machine.TileEntityLargePigmentMixer",
             "com.jerry.meklm.common.tile.machine.TileEntityLargeRotaryCondensentrator",
             "com.jerry.meklm.common.tile.machine.TileEntityLargeSolarNeutronActivator",
 
